cd19e40b81e846566a928156470979c823e59b25
Paul Stone <pmstone247@gmail.com>
2014-05-10 19:29:08 -0600

    TravisCI setup

--- app/models/gig_finder.rb
+++ app/models/gig_finder.rb
@@ -19,14 +19,6 @@ class GigFinder
     artist_event_info["resultsPage"]["results"]["event"].each do |x|
       info << x["venue"]["displayName"]
     end
-    p info.uniq
-
-
-    # make call to http://api.songkick.com/api/3.0/artists/42687/calendar.json?apikey=12XsVy8nEO6w6CqL
-    # to get event id
-
-    # grab the eventsHref for each result and make another call to get event data
-
-    # grab event name and event start date from there and return it
+    info
   end
 end
