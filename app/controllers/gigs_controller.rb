8e7d61a2db797c37dd945f22a68e398d78bee25a
Paul Stone <pmstone247@gmail.com>
2014-07-04 14:37:08 -0600

    refactored gigs_controller

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -3,12 +3,11 @@ class GigsController < ApplicationController
   before_filter :authenticate
 
   def index
-    gigs = GigFinder.new
     artist_name = params[:band_name]
-    artist_id = gigs.find_artist_id(artist_name)
-    @display_name = gigs.find_artist_name(artist_name)
-    @gigs = gigs.find_events(artist_id)
-    @image_url = gigs.find_image(artist_name)
+    artist_id = GigFinder.new.find_artist_id(artist_name)
+    @display_name = GigFinder.new.find_artist_name(artist_name)
+    @gigs = GigFinder.new.find_events(artist_id)
+    @image_url = GigFinder.new.find_image(artist_name)
 
     # remote = Songkickr::Remote.new API_KEY
     # @results = remote.events(type: "concert", artists: params[:band_name])
