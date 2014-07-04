ae224cb56eb0531977373dca6438cbaff239f5e2
Paul Stone <pmstone247@gmail.com>
2014-07-04 13:58:46 -0600

    refactor

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,10 +5,11 @@ class VotesController < ApplicationController
   def new
     @gig_id = params[:gig_id]
     @vote = Vote.new
-    @display_name = params[:artist]
-    @venue = params[:venue]
-    @location = params[:location]
+    display_name = params[:artist]
+    venue = params[:venue]
+    location = params[:location]
     @song_list = SongFinder.new.find_songs_by_artist(params[:artist])
+    @show_info = {display_name: display_name, venue: venue, location: location}
   end
 
   def create
@@ -16,11 +17,11 @@ class VotesController < ApplicationController
     artist_name = params[:artist]
 
     songs.each do |song|
-      @vote = Vote.new(user_id: @current_user.id,
-                       song_name: song,
-                       artist_name: artist_name,
-                       gig_id: params[:gig_id])
-      @vote.save
+      vote = Vote.new(user_id: @current_user.id,
+                      song_name: song,
+                      artist_name: artist_name,
+                      gig_id: params[:gig_id])
+      vote.save
     end
     flash[:notice] = "Your votes have been submitted!"
     redirect_to votes_for_gig_path(gig_id: params[:gig_id])
@@ -31,15 +32,14 @@ class VotesController < ApplicationController
     @name = []
     gig_id = params[:gig_id]
     @show_votes = Vote.where(gig_id: gig_id).group(:artist_name, :song_name).count
-    @show_votes.each do |array, count|
-       @name << array.first
+    @show_votes.each do |array, _|
+      @name << array.first
     end
   end
 
   def votes_for_user
-    user_id = @current_user.id
+    user_id = current_user.id
     @user_votes = Vote.where(user_id: user_id).group(:artist_name, :song_name).count
-
   end
 
   def show
