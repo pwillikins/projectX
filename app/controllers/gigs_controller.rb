b842b88698bd2f5b56243c99da8c462dca2bfaef
Paul Stone <pmstone247@gmail.com>
2014-07-22 16:34:33 -0600

    added untracked files that were left out of previous commit

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -9,9 +9,14 @@ class GigsController < ApplicationController
       flash[:notice] = "You must enter a valid artist name"
     else
       artist = ArtistInfo.new(artist_name)
-      @gigs = GigFinder.new.find_gigs_for_artist(artist_name)
-      @image_url = artist.find_artist_image
       @display_name = artist.find_display_name
+      if @display_name.nil?
+        redirect_to index_path
+        flash[:notice] = "You must enter a valid artist name"
+      else
+        @image_url = artist.find_artist_image
+        @gigs = GigFinder.new.find_gigs_for_artist(artist_name)
+      end
     end
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -8,9 +8,4 @@ describe Vote do
     expect(vote.artist_name).to eq "Pepper"
     expect(vote.song_name).to eq "Ashes"
   end
-
-  # it "cannot create a valid vote" do
-  #   vote = Vote.create(song_name: "")
-  #   expect(vote).to_not be_valid
-  # end
 end
